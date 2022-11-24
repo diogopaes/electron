1443d11d0fe3039a338da34f194c8d2abc88a745
Diogo <diogopaesdev@gmail.com>
2022-11-24 01:57:15 -0300

    feat: twind css

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,5 @@
+import './styles/global.css';
+
 export function App() {
-  return <h1 style={{ fontFamily: 'inter' }}>Hello World</h1>
+  return <h1 className="text-violet-800 text-4xl">Hello World</h1>
 }
