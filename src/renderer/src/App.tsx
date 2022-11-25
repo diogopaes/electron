5b1fe01139afeaefaac6672ba4905ba932702bfa
Diogo <diogopaesdev@gmail.com>
2022-11-25 01:13:05 -0300

    feat: aplication components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,18 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
 export function App() {
-  return <h1 className="text-violet-800 text-4xl">Hello World</h1>
+  return (
+    <div className="h-screen w-screen text-rotion-100 flex">
+      <Sidebar />
+      <div className="flex-1 flex flex-col max-h-screen">
+        <Header />
+
+        <main className="flex-1 flex items-center justify-center text-rotion-400">
+          Selecione ou crie um documento.
+        </main>
+      </div>
+    </div>
+  )
 }
